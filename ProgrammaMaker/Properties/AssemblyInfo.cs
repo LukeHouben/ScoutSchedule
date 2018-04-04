67408bdf26a238d1d04c2d18f07e5e4177ce5c71
Luke Houben <luke@lukehouben.nl>
2018-04-04 21:22:53 +0200

    Changed project name and build name to the correct one + added icon in windows

--- ProgrammaMaker/Properties/AssemblyInfo.cs
+++ ProgrammaMaker/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ProgrammaMaker")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("ScoutSchedule")]
+[assembly: AssemblyDescription("Maak nu makkelijk een online programma met ScoutSchedule!")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ProgrammaMaker")]
+[assembly: AssemblyProduct("ScoutSchedule")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
